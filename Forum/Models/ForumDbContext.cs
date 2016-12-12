8b30b98f3aac854a469fc57a033d51ed99c73bb8
yordank <yordankp@gmail.com>
2016-12-12 02:56:06 +0200

    1.Create a New ASP.NET MVC Application 2.Setup the General Layout of the Blog 3.Edit Class Structure

---
+++ Forum/Models/ForumDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Forum.Models
+{
+  
+
+    public class ForumDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ForumDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ForumDbContext Create()
+        {
+            return new ForumDbContext();
+        }
+    }
+}
